c04458e5b34f4eff60de366058be85dd4950600a
Fedchenko Serhii <spnedyak@gmail.com>
2022-06-09 23:42:27 +0300

    Task 10 changes
    - everything except pagination.

--- force-app/main/default/lwc/positionListLwc/positionListLwc.js
+++ force-app/main/default/lwc/positionListLwc/positionListLwc.js
@@ -2,17 +2,52 @@ import { LightningElement, wire, api } from 'lwc';
 import { getObjectInfo, getPicklistValues } from 'lightning/uiObjectInfoApi';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import { NavigationMixin } from 'lightning/navigation';
-import { updateRecord } from 'lightning/uiRecordApi';
-import { reduceErrors } from 'c/ldsUtils';
 import POSITION__C_OBJECT from '@salesforce/schema/Position__c';
 import STATUS__C_FIELD from '@salesforce/schema/Position__c.Status__c';
 import getPositions from '@salesforce/apex/PositionListLwcController.getPositions';
+import updatePositions from '@salesforce/apex/PositionListLwcController.updatePositions';
+
 
 export default class PositionListLwc extends NavigationMixin(LightningElement) {
-    selectedStatus = 'Open';
-    picklistValues = [];
-    filterPicklistValues = [];
-    selectedPositions = [];
+
+    buttonAccessibility = true; //boolean variable to store "disabled" attribute of "Save" button to be able to enable it when user will change status of record in the page table
+    selectedFilterOption = 'Open'; //string variable to store status picklist filter selected option with default value in it
+    showTable = true; //boolean variable for hiding datatable if there are no records for selected status filter picklist option
+    picklistValues = []; //array to store picklist options for Status__c field of Position__c object received from org. Used for Position status table cell
+    filterPicklistValues = []; //array to store options for status picklist filter if there is a need to add options to existing at Status__c field options of Position__c object at org
+    selectedPositions = []; //array to store position records received from the org and display in the page table
+    modifiedPositions = []; //clone of selectedPositions array to store changes of Position status cell in page table
+
+    //Loading page with default value of status picklist filter
+    connectedCallback() {
+		this.loadPositions(this.selectedFilterOption);
+	}
+
+    //Function for cloning array of objects with primitives (position records)
+    cloneArrayOfObjects(array){
+        let clonedArray = [];
+        for(let i=0; i<array.length; i++){
+            let clonedObjectOfArray = {...array[i]};
+            clonedArray.push(clonedObjectOfArray);
+        }
+        return clonedArray;
+    }
+    
+    //Function for showing page message
+    showMessage(customTitle = '', customMessage = '', customVariant = 'info'){
+        const evt = new ShowToastEvent({
+            title: customTitle,
+            message: customMessage,
+            variant: customVariant,
+        });
+        this.dispatchEvent(evt);
+    }
+
+    /*
+    Receiving picklist options for Status__c field of Position__c object from org,
+    composing filterPicklistValues array,
+    showing error message if there was an error during loading picklist options for Status__c field of Position__c object from org
+    */
     @wire( getObjectInfo, { objectApiName:  POSITION__C_OBJECT })
     objectInfo;
     @wire( getPicklistValues, { recordTypeId: '$objectInfo.data.defaultRecordTypeId', fieldApiName: STATUS__C_FIELD } )
@@ -27,70 +62,106 @@ export default class PositionListLwc extends NavigationMixin(LightningElement) {
             this.additionalPicklistValues = [{value: 'All', label: 'All'}];
             this.filterPicklistValues = [...this.additionalPicklistValues, ...this.picklistValues];
         } else if ( error ) {
-            this.loadingPicklistValuesErrors = reduceErrors(error);
+            this.showMessage('There was a problem loading options for status filter/picklist from database', '', 'error');
         }
-    }
-    
-    handleChange(event) {
-        this.selectedStatus = event.detail.value;
-        getPositions ({
-            selectedStatus: this.selectedStatus
-        })
-            .then (data => {
-                console.log(data.length);
-                this.selectedPositions = data;
-            })
-            .catch(error => {
-				this.loadingSelectedPositionsErrors = reduceErrors(error);
-			});
     }
 
     /*
-    updatePositions(){
-        const fields = this.selectedPositions[0];
-        const recordsInput = {fields};
-        console.log(recordsInput);
-        console.log(typeof STATUS__C_FIELD.fieldApiName);
-        updateRecord(recordsInput)
-            .then(() => {
-                this.dispatchEvent(
-                    new ShowToastEvent({
-                        title: 'Success',
-                        message: 'Case Updated',
-                        variant: 'success'
-                    })
-                );
-            })
-            .catch(error => {
-                console.log(error);
-            });
-    }
+    Function for:
+    - receiving positions records from org;
+    - cloning received array;
+    - hiding table and showing warning message if there are no positions records for selected status picklist filter option;
+    - showing error message if there was error during loading positions records from org.
     */
-
-    connectedCallback() {
-		getPositions ({
-            selectedStatus: this.selectedStatus
+    loadPositions(statusFilterValue){
+        getPositions ({
+            selectedFilterOption: statusFilterValue
         })
             .then (data => {
                 this.selectedPositions = data;
+                this.modifiedPositions = this.cloneArrayOfObjects(this.selectedPositions);
+                if (this.selectedPositions.length === 0){
+                    this.showTable = false;
+                    this.showMessage(`There are no positions with status "${statusFilterValue}"`, '', 'warning');
+                } else {
+                    this.showTable = true;
+                }
             })
-            .catch(error => {
-				this.loadingSelectedPositionsErrors = reduceErrors(error);
+            .catch (error => {
+				this.showMessage('There was a problem loading position records from database', '', 'error');
 			});
-	}
+        
+    }
 
+    //Function to navigate to position record details page at new browser tab if Position name clicked
     navigateToPositionRecord(event){
         event.preventDefault();
-        let linkId = event.target.getAttribute('id');
-        let navigatedRecordId = linkId.substring(0, linkId.indexOf('-'));
+        const id = event.target.closest('tr').dataset.positionId;
         this[NavigationMixin.GenerateUrl]({
             type: 'standard__recordPage',
             attributes: {
-                recordId: navigatedRecordId,
+                recordId: id,
                 actionName: 'view'
             }
         }).then(url => {
             window.open(url);
         })
+    }
+    
+    //Function for handling status picklist filter change
+    handleFilterChange(event) {
+        this.selectedFilterOption = event.detail.value;
+        this.loadPositions(this.selectedFilterOption);
+    }
+
+    //Function for handling status picklists changes in the page table
+    handleStatusChange(event){
+        this.buttonAccessibility = false;
+        const selectedStatus = event.detail.value;
+        const id = event.target.closest('tr').dataset.positionId;
+        for(let i=0; i<this.modifiedPositions.length; i++){
+            if(this.modifiedPositions[i].Id === id){
+                this.modifiedPositions[i].Status__c = selectedStatus;
+            }
+        }
+    }
+
+    /*
+    Function for handling onclick event of 'Save' button that is:
+    - compositing array of position records that were modified and need to be updated as difference between selectedPositions array and modifiedPositions array;
+    - showing page message if there were no any records changes;
+    - updating records with updatePositions method from apex controller;
+    - showing 'success' message and reloading page with current selectedFilterOption if records were saved successfully;
+    - showing 'error' message if there were any errors during records updating.
+    */
+    updateRecords(){
+        let positionsToUpdate = [];
+        for(let i=0; i<this.modifiedPositions.length; i++){
+            let positionToUpdate = {};
+            if(this.modifiedPositions[i].Status__c !== this.selectedPositions[i].Status__c){
+                positionToUpdate.Id = this.modifiedPositions[i].Id;
+                positionToUpdate.Status__c = this.modifiedPositions[i].Status__c;
+                positionsToUpdate.push(positionToUpdate);
+            }
+        }
+        if(positionsToUpdate.length === 0){
+            this.showMessage('Was not modified any status', '', 'warning');
+        } else {
+            updatePositions ({
+                positions: positionsToUpdate
+            })
+                .then (data => {
+                    if (data === 'Success') {
+                        this.showMessage('Changes were saved successfully', '', 'success');
+                        this.connectedCallback();
+                    } else if (data === 'Error') {
+                        this.showMessage('There was an error updating records. Please try again', '', 'error');
+                    }
+                    
+                })
+                .catch (error => {
+                    this.showMessage('There was an error updating records. Please try again', '', 'error');
+                });
+        }
     }
 }
